c65fb9ea878063bf9024bb17ab8c488682af0c09
goku-Tech-ux <mg4351724@gmail.com>
2026-07-23 14:12:20 +0530

    Create Patern1.java

---
+++ Patern1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER OF LINES");
+        int n = sc.nextInt();
+       for(int i=n;i>=0;i--)
+       {
+           for(int j=i;j<n;j++)
+           {
+               System.out.print("* ");
+           
+       }System.out.println();
+    }
+}}
